51f776878444181460d6d9b404f4fa7e37e81b2b
Théo <thp2909@gmail.com>
2026-05-12 20:46:35 +0200

    Ajout local storage JS pour conserver paramètres sélectionnes dans menu daccessbilite, dune page à l'autre

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-
-
-
-
 // On attend que la page soit totalement chargée
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -31,8 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
     btnMenuNav.addEventListener("click", () => {
         const menu_is_open = liste_nav.classList.toggle("menu-ouvert");
         header.classList.toggle("header-menu-ouvert"); // Ajoute ou retire la classe "menu-ouvert" au header pour changer son style
-            // Met à jour l'a   ttribut aria-expanded pour l'accessibilité
-        btnMenuNav.setAttribute("aria-expanded", menu_is_open);
+            
+        btnMenuNav.setAttribute("aria-expanded", menu_is_open); // Met à jour l'attribut aria-expanded pour l'accessibilité
         // 5. On change le texte du bouton selon l'état !
         if (menu_is_open) {
             btnMenuNav.textContent = "✖ Fermer";
@@ -50,26 +46,66 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const btnMenuAccessibilite = document.getElementById("btn_menu_accessibilite");
     const listeAccessibilite = document.getElementById("liste_accessibilite");
+
     btnMenuAccessibilite.addEventListener("click", () => {
         const isOpen = listeAccessibilite.classList.toggle("menu-accessibilite-ouvert"); //
         btnMenuAccessibilite.setAttribute("aria-expanded", isOpen);
     });
 
+
+    // 1. On vérifie la taille du texte
+    if (localStorage.getItem("texte-agrandi") === "actif") {
+        document.documentElement.classList.add("texte-agrandi");
+        btnTailleTexte.setAttribute("aria-pressed", "true");
+    }
+
+    // 2. On vérifie l'interligne
+    if (localStorage.getItem("interligne-agrandi") === "actif") {
+        document.body.classList.add("interligne-agrandi");
+        btnInterligne.setAttribute("aria-pressed", "true");
+    }
+
+    // 3. On vérifie le mode sombre
+    if (localStorage.getItem("theme-sombre") === "actif") {
+        document.body.classList.add("theme-sombre");
+        btnContraste.setAttribute("aria-pressed", "true");
+    }
+
+
     // 1. Taille du texte (sur la balise <html>)
     btnTailleTexte.addEventListener("click", () => {
         const isActive = document.documentElement.classList.toggle("texte-agrandi");
         btnTailleTexte.setAttribute("aria-pressed", isActive); // Pour les lecteurs d'écran
+
+        // On sauvegarde le choix
+        if (isActive) {
+            localStorage.setItem("texte-agrandi", "actif");
+        } else {
+            localStorage.removeItem("texte-agrandi"); // On efface la note si désactivé
+        }
     });
 
     // 2. Interligne (sur la balise <body>)
     btnInterligne.addEventListener("click", () => {
         const isActive = document.body.classList.toggle("interligne-agrandi");
         btnInterligne.setAttribute("aria-pressed", isActive);
+
+        if (isActive) {
+            localStorage.setItem("interligne-agrandi", "actif");
+        } else {
+            localStorage.removeItem("interligne-agrandi");
+        }
     });
 
     // 3. Mode Sombre / Contraste (sur la balise <body>)
     btnContraste.addEventListener("click", () => {
         const isActive = document.body.classList.toggle("theme-sombre");
         btnContraste.setAttribute("aria-pressed", isActive);
+
+        if (isActive) {
+            localStorage.setItem("theme-sombre", "actif");
+        } else {
+            localStorage.removeItem("theme-sombre");
+        }
     });
 });
